f4816f7a80c0e82cc77275e6160f236ff3586f93
rpletka <rpletka@vmware.com>
2020-07-24 08:38:26 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+# Configure the VMware NSX-T Provider
+provider "nsxt" {
+    host = var.nsxIP
+    username = var.nsxUser
+    password = var.nsxPassword
+    allow_unverified_ssl = true
+}
+
+
+resource "nsxt_policy_group" "MySQLClient" {
+  display_name = "MySQLClient"
+  description  = "MySQLClient Group provisioned by Terraform"
+  criteria {
+        condition {
+            key         = "Tag"
+            member_type = "VirtualMachine"
+            operator    = "EQUALS"
+            value       = "Role|MySQLClient"
+        }
+    }
+}
+
+resource "nsxt_policy_group" "MySQLServer" {
+  display_name = "MySQLServer"
+  description  = "MySQLServer Group provisioned by Terraform"
+  criteria {
+        condition {
+            key         = "Tag"
+            member_type = "VirtualMachine"
+            operator    = "EQUALS"
+            value       = "Role|MySQLServer"
+        }
+    }
+}
+resource "nsxt_policy_group" "WebServer" {
+  display_name = "WebServer"
+  description  = "WebServer Group provisioned by Terraform"
+  criteria {
+        condition {
+            key         = "Tag"
+            member_type = "VirtualMachine"
+            operator    = "EQUALS"
+            value       = "Role|WebServer"
+        }
+    }
+  }
+}
